865c56b9c7ff5b33bf2491447b12f91ee8da3df1
Dan Lin <dlin2102@gmail.com>
2017-03-10 14:30:06 -0500

    added more bootstrap

--- js/app.js
+++ js/app.js
@@ -1,6 +1,47 @@
 angular
-  .module("touristApp", ["ui.router"])
+  .module("touristApp", [
+    "ui.router",
+    "ngResource"])
+
   .config([
     "$stateProvider",
     RouterFunction
-  ])
+  ]);
+
+  .factory("ActivityFactory",
+  ["$resource",
+ActivityFactoryFunction]);
+
+  .controller("ActivityShowController", [
+    "ActivityFactory",
+    "stateParams",
+    ActivityShowController
+  ]);
+
+function ActivityFactoryFunction($resource){
+  return $resource("http://localhost:3000/activities/:id");
+}
+
+function ActivityShowControllerFunction(ActivityFactory, $stateParams){
+  this.activity = ActivityFactory.get({id: $stateParams.id});
+}
+
+$stateProvider
+  .state("activityIndex", {
+    url: "/activities",
+    templateUrl: "js/ng-views/index.html",
+    controller: "ActivityIndexController",
+    controllerAs: "vm"
+  })
+  .state("activityNew", {
+    url: "/activities/new",
+    templateUrl: "js.ng-views/new.html",
+    controller: "ActivityShowController",
+    controllerAs: "vm"
+  })
+  .state("activityShow", {
+    url: "/activities/:id",
+    templateUrl: "js/ng-views/show.html",
+    controller: "ActivityShowController",
+    controllerAs: "vm"
+  });
