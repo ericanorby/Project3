c44fef5d3573998c7f42115a18c0fda7b9ed9bbd
Dan Lin <dlin2102@gmail.com>
2017-03-14 15:36:20 -0400

    modal for add button w/o styling

--- js/app.js
+++ js/app.js
@@ -1,127 +1,167 @@
 angular
-  .module("touristApp", [
-    "ui.router",
-    "ngResource"
-  ])
+    .module("touristApp", [
+        "ui.router",
+        "ngResource",
+        "angularModalService"
+    ])
 
-  .config([
-    "$stateProvider",
-    "$urlRouterProvider",
-    RouterFunction
-  ])
+    .config([
+        "$stateProvider",
+        "$urlRouterProvider",
+        RouterFunction
+    ])
 
-  .factory("LocationFactory", ["$resource",
-    LocationFactoryFunction
-  ])
+    .factory("LocationFactory", ["$resource",
+        LocationFactoryFunction
+    ])
 
-  .factory("ActivityFactory", ["$resource",
-    ActivityFactoryFunction
-  ])
+    .factory("ActivityFactory", ["$resource",
+        ActivityFactoryFunction
+    ])
 
-  .controller("HomeController", [
-    "LocationFactory",
-    "$stateParams",
-    "$state",
-    HomeControllerFunction
-  ])
+    .controller("HomeController", [
+        "LocationFactory",
+        "$stateParams",
+        "$state",
+        HomeControllerFunction
+    ])
 
-  .controller("LocationShowController", [
-    "LocationFactory",
-    "ActivityFactory",
-    "$stateParams",
-    "$state",
-    LocationShowControllerFunction
-  ])
+    .controller("LocationShowController", [
+        "LocationFactory",
+        "ActivityFactory",
+        "$stateParams",
+        "$state",
+        "ModalService",
+        LocationShowControllerFunction
+    ])
 
-  .controller("ActivityShowController", [
-    "ActivityFactory",
-    "$stateParams",
-    ActivityShowControllerFunction
-  ])
+    .controller("ActivityShowController", [
+        "ActivityFactory",
+        "$stateParams",
+        ActivityShowControllerFunction
+    ])
+
+    .controller("ActivityCreateModalController", [
+      "ActivityFactory",
+      "$stateParams",
+      "$scope",
+      "close",
+      ActivityCreateModalControllerFunction
+    ])
 
 
 function RouterFunction($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state("home", {
-      url: "/home",
-      templateUrl: "js/ng-views/home.html",
-      controller: "HomeController",
-      controllerAs: "vm"
-    })
-    .state("location", {
-      url: "/locations/:id",
-      templateUrl: "js/ng-views/location/show.html",
-      controller: "LocationShowController",
-      controllerAs: "vm"
-    })
-    .state("location.activities", {
-      url: "/:activity_id",
-      templateUrl: "js/ng-views/activity/show.html",
-      controller: "ActivityShowController",
-      controllerAs: "vm"
-    })
+    $stateProvider
+        .state("home", {
+            url: "/home",
+            templateUrl: "js/ng-views/home.html",
+            controller: "HomeController",
+            controllerAs: "vm"
+        })
+        .state("location", {
+            url: "/locations/:id",
+            templateUrl: "js/ng-views/location/show.html",
+            controller: "LocationShowController",
+            controllerAs: "vm"
+        })
+        .state("location.activities", {
+            url: "/:activity_id",
+            templateUrl: "js/ng-views/activity/show.html",
+            controller: "ActivityShowController",
+            controllerAs: "vm"
+        })
 
-  $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/home');
 }
 
 function LocationFactoryFunction($resource) {
-  return $resource("http://localhost:3000/locations/:id.json");
+    return $resource("http://localhost:3000/locations/:id.json");
 }
 
 function ActivityFactoryFunction($resource) {
-  return $resource("http://localhost:3000/locations/:location_id/activities/", {
-    location_id: '@location_id',
-    id: '@activity_id'
-  },
-  {
-    'create':   {method:'POST'},
-    'query':  {method:'GET', isArray:true},
-  });
+    return $resource("http://localhost:3000/locations/:location_id/activities/", {
+        location_id: '@location_id',
+        id: '@activity_id'
+    }, {
+        'create': {
+            method: 'POST'
+        },
+        'query': {
+            method: 'GET',
+            isArray: true
+        },
+    });
 }
 
 function HomeControllerFunction(LocationFactory, $stateParams, $state) {
 
-  var options = {
-    types: ['(cities)']
-  }
-  var input = document.getElementById('search-box');
-  var autocomplete = new google.maps.places.Autocomplete(input, options);
-
-  this.create = function(){
-    var data = {
-      name: input.value
+    var options = {
+        types: ['(cities)']
     }
-    LocationFactory.save(data, function(location){
-      $state.go('location', {
-        id: location.id
-      })
-    });
-   };
+    var input = document.getElementById('search-box');
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    this.create = function() {
+        var data = {
+            name: input.value
+        }
+        LocationFactory.save(data, function(location) {
+            $state.go('location', {
+                id: location.id
+            })
+        });
+    };
 
 }
 
-function LocationShowControllerFunction(LocationFactory, ActivityFactory, $stateParams) {
-  var vm = this;
-  this.location = LocationFactory.get({id: $stateParams.id})
-  this.activities = ActivityFactory.query({location_id: $stateParams.id})
-  this.addActivity = function() {
-    var data = {
-      name: this.activityInput,
-      address: 'test adress',
-      category: 'Stuff',
-      photo_url: 'http://google.com',
-      website_url: 'http://yahoo.com',
-      description: 'description'
-    }
-    ActivityFactory.create({location_id: $stateParams.id}, data, function(result){
-      vm.activities.push(result);
+function LocationShowControllerFunction(LocationFactory, ActivityFactory, $stateParams, $state, ModalService) {
+    var vm = this;
+    this.location = LocationFactory.get({
+        id: $stateParams.id
     })
-  }
+    this.activities = ActivityFactory.query({
+        location_id: $stateParams.id
+    })
+    this.addActivity = function() {
+        ModalService.showModal({
+            templateUrl: "js/ng-views/activity/activity-creation-modal.html",
+            controller: "ActivityCreateModalController"
+        }).then(function(modal) {
+            // The modal object has the element built, if this is a bootstrap modal
+            // you can call 'modal' to show it, if it's a custom modal just show or hide
+            // it as you need to.
+            // modal.element.modal();
+            // modal.close.then(function(result) {
+            //     $scope.message = result ? "You said Yes" : "You said No";
+            // });
+        });
+
+    }
 }
 
 function ActivityShowControllerFunction(ActivityFactory, $stateParams) {
-  // this.activityDetails = activities
-  //   .filter(function(activity) {
-  //     return activity.id == $stateParams.activity_id
-  //   })[0];
+    // this.activityDetails = activities
+    //   .filter(function(activity) {
+    //     return activity.id == $stateParams.activity_id
+    //   })[0];
+}
+
+function ActivityCreateModalControllerFunction(ActivityFactory, $stateParams, $scope, close) {
+  $scope.addActivity = function(){
+    var data = {
+      name: $scope.activityName,
+      address: $scope.activityAddress,
+      category: $scope.activityCategory,
+      photo_url: $scope.activityPhoto,
+      website_url: $scope.activityWebsite,
+      description: $scope.activityDescription
+    }
+    ActivityFactory.create({location_id: $stateParams.id}, data, function(result){
+      close(result, 0);
+      // $scope.activities.push(result);
+    })
+  }
+  $scope.closeModal = function(){
+    close('close', 0);
+  }
 }
