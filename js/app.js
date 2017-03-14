5ac8fad6dc0573383e92b94da74d6691cd076998
Erica Norby <ericanorby@gmail.com>
2017-03-14 17:12:10 -0400

    minor style edits for location show page

--- js/app.js
+++ js/app.js
@@ -1,178 +1,184 @@
 angular
-    .module("touristApp", [
-        "ui.router",
-        "ngResource",
-        "angularModalService"
-    ])
+  .module("touristApp", [
+    "ui.router",
+    "ngResource",
+    "angularModalService"
+  ])
 
-    .config([
-        "$stateProvider",
-        "$urlRouterProvider",
-        RouterFunction
-    ])
+  .config([
+    "$stateProvider",
+    "$urlRouterProvider",
+    RouterFunction
+  ])
 
-    .factory("LocationFactory", ["$resource",
-        LocationFactoryFunction
-    ])
+  .factory("LocationFactory", ["$resource",
+    LocationFactoryFunction
+  ])
 
-    .factory("ActivityFactory", ["$resource",
-        ActivityFactoryFunction
-    ])
+  .factory("ActivityFactory", ["$resource",
+    ActivityFactoryFunction
+  ])
 
-    .controller("HomeController", [
-        "LocationFactory",
-        "$stateParams",
-        "$state",
-        HomeControllerFunction
-    ])
+  .controller("HomeController", [
+    "LocationFactory",
+    "$stateParams",
+    "$state",
+    HomeControllerFunction
+  ])
 
-    .controller("LocationShowController", [
-        "LocationFactory",
-        "ActivityFactory",
-        "$stateParams",
-        "$state",
-        "ModalService",
-        LocationShowControllerFunction
-    ])
+  .controller("LocationShowController", [
+    "LocationFactory",
+    "ActivityFactory",
+    "$stateParams",
+    "$state",
+    "ModalService",
+    LocationShowControllerFunction
+  ])
 
-    .controller("ActivityShowController", [
-        "ActivityFactory",
-        "$stateParams",
-        ActivityShowControllerFunction
-    ])
+  .controller("ActivityShowController", [
+    "ActivityFactory",
+    "$stateParams",
+    ActivityShowControllerFunction
+  ])
 
-    .controller("ActivityCreateModalController", [
-      "ActivityFactory",
-      "$stateParams",
-      "$scope",
-      "close",
-      ActivityCreateModalControllerFunction
-    ])
+  .controller("ActivityCreateModalController", [
+    "ActivityFactory",
+    "$stateParams",
+    "$scope",
+    "close",
+    ActivityCreateModalControllerFunction
+  ])
 
 
 function RouterFunction($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state("home", {
-            url: "/home",
-            templateUrl: "js/ng-views/home.html",
-            controller: "HomeController",
-            controllerAs: "vm"
-        })
-        .state("location", {
-            url: "/locations/:id",
-            templateUrl: "js/ng-views/location/show.html",
-            controller: "LocationShowController",
-            controllerAs: "vm"
-        })
-        .state("location.activities", {
-            url: "/:activity_id",
-            templateUrl: "js/ng-views/activity/show.html",
-            controller: "ActivityShowController",
-            controllerAs: "vm"
-        })
+  $stateProvider
+    .state("home", {
+      url: "/home",
+      templateUrl: "js/ng-views/home.html",
+      controller: "HomeController",
+      controllerAs: "vm"
+    })
+    .state("location", {
+      url: "/locations/:id",
+      templateUrl: "js/ng-views/location/show.html",
+      controller: "LocationShowController",
+      controllerAs: "vm"
+    })
+    // .state("activityShow", {
+    //   url: "locations/:id/activities/:activity_id",
+    //   templateUrl: "js/ng-views/activity/show.html",
+    //   controller: "ActivityShowController",
+    //   controllerAs: "vm"
+    // })
 
-    $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/home');
 }
 
 function LocationFactoryFunction($resource) {
-    return $resource("http://localhost:3000/locations/:id.json");
+  return $resource("http://localhost:3000/locations/:id.json");
 }
 
 function ActivityFactoryFunction($resource) {
-    return $resource("http://localhost:3000/locations/:location_id/activities/", {
-        location_id: '@location_id',
-        id: '@activity_id'
-    }, {
-        'create': {
-            method: 'POST'
-        },
-        'query': {
-            method: 'GET',
-            isArray: true
-        },
-    });
+  return $resource("http://localhost:3000/locations/:location_id/activities/", {
+    location_id: '@location_id',
+    id: '@activity_id'
+  }, {
+    'create': {
+      method: 'POST'
+    },
+    'query': {
+      method: 'GET',
+      isArray: true
+    },
+  });
 }
 
 function HomeControllerFunction(LocationFactory, $stateParams, $state) {
-    var input = document.getElementById('search-box');
-    var autocomplete = new google.maps.places.Autocomplete(input, {types: ['(cities)']});
-    google.maps.event.addListener(autocomplete, 'place_changed', function () {
-          var place = autocomplete.getPlace();
 
-          IsplaceChange = true;
-      });
+  var input = document.getElementById('search-box');
+  var autocomplete = new google.maps.places.Autocomplete(input, {
+    types: ['(cities)']
+  });
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    var place = autocomplete.getPlace();
 
-      $("#search-box").keydown(function () {
-          IsplaceChange = false;
-      });
+    IsplaceChange = true;
+  });
 
-    this.location = new LocationFactory();
+  $("#search-box").keydown(function() {
+    IsplaceChange = false;
+  });
 
-    this.create = function() {
-      if (IsplaceChange == false) {
-          $("#search-box").val('');
-          alert("Please enter valid location");
-      } else {
+  this.location = new LocationFactory();
+
+  this.create = function() {
+    if (IsplaceChange == false) {
+      $("#search-box").val('');
+      alert("Please enter valid location");
+    }
+    else {
       var userInput = $("#search-box").val();
       console.log(userInput)
       this.location.name = userInput
       this.location.$save(function(location) {
-        $state.go('location', {
-          id: location.id
+          $state.go('location', {
+            id: location.id
+          })
         })
-      })
-    }
     };
+  }
 }
 
-function LocationShowControllerFunction(LocationFactory, ActivityFactory, $stateParams, $state, ModalService) {
+  function LocationShowControllerFunction(LocationFactory, ActivityFactory, $stateParams, $state, ModalService) {
     var vm = this;
     this.location = LocationFactory.get({
-        id: $stateParams.id
+      id: $stateParams.id
     })
     this.activities = ActivityFactory.query({
-        location_id: $stateParams.id
+      location_id: $stateParams.id
     })
     this.addActivity = function() {
-        ModalService.showModal({
-            templateUrl: "js/ng-views/activity/activity-creation-modal.html",
-            controller: "ActivityCreateModalController"
-        }).then(function(modal) {
-            // The modal object has the element built, if this is a bootstrap modal
-            // you can call 'modal' to show it, if it's a custom modal just show or hide
-            // it as you need to.
-            // modal.element.modal();
-            // modal.close.then(function(result) {
-            //     $scope.message = result ? "You said Yes" : "You said No";
-            // });
-        });
+      ModalService.showModal({
+        templateUrl: "js/ng-views/activity/activity-creation-modal.html",
+        controller: "ActivityCreateModalController"
+      }).then(function(modal) {
+        // The modal object has the element built, if this is a bootstrap modal
+        // you can call 'modal' to show it, if it's a custom modal just show or hide
+        // it as you need to.
+        // modal.element.modal();
+        // modal.close.then(function(result) {
+        //     $scope.message = result ? "You said Yes" : "You said No";
+        // });
+      });
 
     }
-}
+  }
 
-function ActivityShowControllerFunction(ActivityFactory, $stateParams) {
+  function ActivityShowControllerFunction(ActivityFactory, $stateParams) {
     // this.activityDetails = activities
     //   .filter(function(activity) {
     //     return activity.id == $stateParams.activity_id
     //   })[0];
-}
+  }
 
-function ActivityCreateModalControllerFunction(ActivityFactory, $stateParams, $scope, close) {
-  $scope.addActivity = function(){
-    var data = {
-      name: $scope.activityName,
-      address: $scope.activityAddress,
-      category: $scope.activityCategory,
-      photo_url: $scope.activityPhoto,
-      website_url: $scope.activityWebsite,
-      description: $scope.activityDescription
+  function ActivityCreateModalControllerFunction(ActivityFactory, $stateParams, $scope, close) {
+    $scope.addActivity = function() {
+      var data = {
+        name: $scope.activityName,
+        address: $scope.activityAddress,
+        category: $scope.activityCategory,
+        photo_url: $scope.activityPhoto,
+        website_url: $scope.activityWebsite,
+        description: $scope.activityDescription
+      }
+      ActivityFactory.create({
+        location_id: $stateParams.id
+      }, data, function(result) {
+        close(result, 0);
+        // $scope.activities.push(result);
+      })
     }
-    ActivityFactory.create({location_id: $stateParams.id}, data, function(result){
-      close(result, 0);
-      // $scope.activities.push(result);
-    })
-  }
-  $scope.closeModal = function(){
-    close('close', 0);
-  }
-}
+    $scope.closeModal = function() {
+        close('close', 0);
+      }
+    }
